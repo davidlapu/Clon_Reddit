ff0707aa4dac080c20b96b3dabc7dcbbb16a9f90
David Lama Pulido <david.lama.7e3@itb.cat>
2021-02-12 12:12:33 +0100

    lunchScreen

--- app/src/main/java/cat/itb/clonreddit/activities/MainActivity.java
+++ app/src/main/java/cat/itb/clonreddit/activities/MainActivity.java
@@ -10,6 +10,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.Theme_ClonReddit);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
