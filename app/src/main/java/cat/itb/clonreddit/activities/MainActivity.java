04526cb0b4d9c7774d3068dfcff1529adb7dc6f7
David Lama Pulido <david.lama.7e3@itb.cat>
2021-02-12 11:00:01 +0100

    Initial commit

---
+++ app/src/main/java/cat/itb/clonreddit/activities/MainActivity.java
@@ -0,0 +1,16 @@
+package cat.itb.clonreddit.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import cat.itb.clonreddit.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
