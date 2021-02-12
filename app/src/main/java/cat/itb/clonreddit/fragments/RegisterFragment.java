92de938f893bc2f1a0fdc6466345f7f04af0304f
Daniel Acosta Romero <daniel.acosta.7e3@itb.cat>
2021-02-12 11:27:21 +0100

    12/02/2021  11:26

--- app/src/main/java/cat/itb/clonreddit/fragments/RegisterFragment.java
+++ app/src/main/java/cat/itb/clonreddit/fragments/RegisterFragment.java
@@ -1,16 +1,22 @@
 package cat.itb.clonreddit.fragments;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.text.Spannable;
+import android.text.SpannableStringBuilder;
+import android.text.style.ForegroundColorSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import cat.itb.clonreddit.R;
 
 public class RegisterFragment extends Fragment {
+    TextView policyTextView;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -18,9 +24,25 @@ public class RegisterFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_register, container, false);
+
+        policyTextView = v.findViewById(R.id.policyUserTextView);
+
+        SpannableStringBuilder spannable = new SpannableStringBuilder("By continuing, you agree to our User Agreement and Privacy Policy.");
+        spannable.setSpan(
+                new ForegroundColorSpan(Color.parseColor("#0078EF")),
+                31, // start
+                46, // end
+                Spannable.SPAN_EXCLUSIVE_INCLUSIVE);
+        spannable.setSpan(
+                new ForegroundColorSpan(Color.parseColor("#0078EF")),
+                50,
+                65,
+                Spannable.SPAN_EXCLUSIVE_INCLUSIVE);
+
+        policyTextView.setText(spannable);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false);
+        return v;
     }
 }
