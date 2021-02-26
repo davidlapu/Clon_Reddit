f133dc6d63667aa75adb35c9e466dfa6bc8d5642
David Lama Pulido <david.lama.7e3@itb.cat>
2021-02-26 13:12:42 +0100

    models

--- app/src/main/java/cat/itb/clonreddit/models/Post.java
+++ app/src/main/java/cat/itb/clonreddit/models/Post.java
@@ -1,5 +1,76 @@
 package cat.itb.clonreddit.models;
 
 public class Post {
+    private SubReddit subReddit;
+    private String user, time, title;
+    private int numAwards, imageId, upVotes, commentsNum;
+
+    public Post() {
+    }
+
+    public SubReddit getSubReddit() {
+        return subReddit;
+    }
+
+    public void setSubReddit(SubReddit subReddit) {
+        this.subReddit = subReddit;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getNumAwards() {
+        return numAwards;
+    }
+
+    public void setNumAwards(int numAwards) {
+        this.numAwards = numAwards;
+    }
+
+    public int getImageId() {
+        return imageId;
+    }
+
+    public void setImageId(int imageId) {
+        this.imageId = imageId;
+    }
+
+    public int getUpVotes() {
+        return upVotes;
+    }
+
+    public void setUpVotes(int upVotes) {
+        this.upVotes = upVotes;
+    }
+
+    public int getCommentsNum() {
+        return commentsNum;
+    }
+
+    public void setCommentsNum(int commentsNum) {
+        this.commentsNum = commentsNum;
+    }
+
 
 }
--- app/src/main/java/cat/itb/clonreddit/models/SubReddit.java
+++ app/src/main/java/cat/itb/clonreddit/models/SubReddit.java
@@ -2,5 +2,29 @@ package cat.itb.clonreddit.models;
 
 public class SubReddit {
     private String title;
-    //private Image
+    private int imageId;
+
+    public SubReddit() {
+    }
+
+    public SubReddit(String title, int imageId) {
+        this.title = title;
+        this.imageId = imageId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getImageId() {
+        return imageId;
+    }
+
+    public void setImageId(int imageId) {
+        this.imageId = imageId;
+    }
 }
