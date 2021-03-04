1c84a47f5d67fadf8214f52ebb119c139f15d07b
David Lama Pulido <david.lama.7e3@itb.cat>
2021-03-04 11:46:45 +0100

    a

--- app/src/main/java/cat/itb/clonreddit/adapters/PostAdapter.java
+++ app/src/main/java/cat/itb/clonreddit/adapters/PostAdapter.java
@@ -1,7 +1,6 @@
 package cat.itb.clonreddit.adapters;
 
 import android.content.Context;
-import android.content.res.Resources;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
