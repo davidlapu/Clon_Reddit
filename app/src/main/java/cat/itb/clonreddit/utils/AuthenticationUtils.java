ec420ab05630ce917993d85b64535dc7921e4bbd
David Lama Pulido <david.lama.7e3@itb.cat>
2021-03-10 18:16:43 +0100

    a

--- app/src/main/java/cat/itb/clonreddit/utils/AuthenticationUtils.java
+++ app/src/main/java/cat/itb/clonreddit/utils/AuthenticationUtils.java
@@ -1,9 +1,32 @@
 package cat.itb.clonreddit.utils;
 
+import android.app.Activity;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
+import cat.itb.clonreddit.R;
+
 public class AuthenticationUtils {
-/*    public static boolean signInWithEmailAndPassword() {
+/*    public boolean signInWithEmailAndPassword(Activity act, FirebaseAuth mAuth, String email, String password, ) {
+        boolean correctLogin;
+
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this::completeListener);
+
+    }
+
+    private void completeListener(Task<AuthResult> authResultTask) {
+        if (authResultTask.isSuccessful()) {
+
+        } else {
+
+
+        }
 
     }*/
 }
