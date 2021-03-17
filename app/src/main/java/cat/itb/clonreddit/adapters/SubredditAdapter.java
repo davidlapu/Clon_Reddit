071c3cd205f845e69205586b6879263a35621c74
David Lama Pulido <david.lama.7e3@itb.cat>
2021-03-17 13:30:10 +0100

    subreddit loading into recycler

--- app/src/main/java/cat/itb/clonreddit/adapters/SubredditAdapter.java
+++ app/src/main/java/cat/itb/clonreddit/adapters/SubredditAdapter.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -11,6 +12,8 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.android.material.imageview.ShapeableImageView;
+import com.squareup.picasso.Picasso;
 
 import cat.itb.clonreddit.R;
 import cat.itb.clonreddit.models.SubReddit;
@@ -28,7 +31,7 @@ public class SubredditAdapter extends FirebaseRecyclerAdapter<SubReddit, Subredd
     @Override
     public SubredditViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.fragment_bottom_sheet, parent, false); // TODO: cambiar id: fragment_sub_reddit_list
+                .inflate(R.layout.subreddit_item, parent, false);
 
         return new SubredditViewHolder(view);
     }
@@ -40,14 +43,19 @@ public class SubredditAdapter extends FirebaseRecyclerAdapter<SubReddit, Subredd
 
 
     class SubredditViewHolder extends RecyclerView.ViewHolder {
+        private final TextView textViewName;
+        private final ShapeableImageView imageViewSubreddit;
 
 
         public SubredditViewHolder(@NonNull View itemView) {
             super(itemView);
+            textViewName = itemView.findViewById(R.id.imageViewSubredditPost);
+            imageViewSubreddit = itemView.findViewById(R.id.textViewNameSubreddit);
         }
 
         private void bind(SubReddit subReddit) {
-
+            textViewName.setText(subReddit.getTitle());
+            Picasso.with(context).load(subReddit.getImgUrl()).into(imageViewSubreddit);
         }
     }
 }
--- app/src/main/java/cat/itb/clonreddit/fragments/SubRedditListFragment.java
+++ app/src/main/java/cat/itb/clonreddit/fragments/SubRedditListFragment.java
@@ -3,17 +3,26 @@ package cat.itb.clonreddit.fragments;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+
 import cat.itb.clonreddit.R;
+import cat.itb.clonreddit.adapters.PostAdapter;
+import cat.itb.clonreddit.adapters.SubredditAdapter;
+import cat.itb.clonreddit.models.Post;
+import cat.itb.clonreddit.models.SubReddit;
+import cat.itb.clonreddit.utils.ConexionBBDD;
 
 
 public class SubRedditListFragment extends Fragment {
 
-
+    private SubredditAdapter adapter;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +33,25 @@ public class SubRedditListFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_sub_reddit_list, container, false);
         // Inflate the layout for this fragment
+        RecyclerView recyclerView = v.findViewById(R.id.recyclerViewSubrredit);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(requireContext()));
+        FirebaseRecyclerOptions<SubReddit> options = new FirebaseRecyclerOptions.Builder<SubReddit>()
+                .setQuery(ConexionBBDD.getReferenceSubReddit(), SubReddit.class).build();
+        adapter = new SubredditAdapter(options, requireContext());
+        recyclerView.setAdapter(adapter);
         return v;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        adapter.startListening();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        adapter.startListening();
     }
 }
