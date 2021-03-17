0d363c2bfd934eb904a55710a2954dfb8fd2987f
Daniel Acosta Romero <daniel.acosta.7e3@itb.cat>
2021-03-17 13:32:44 +0100

    GOSALO MAMARRASHO

--- app/src/main/java/cat/itb/clonreddit/fragments/SubRedditListFragment.java
+++ app/src/main/java/cat/itb/clonreddit/fragments/SubRedditListFragment.java
@@ -3,27 +3,40 @@ package cat.itb.clonreddit.fragments;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import cat.itb.clonreddit.R;
 
 
 public class SubRedditListFragment extends Fragment {
+    private ImageView closeBTN;
+    private NavController navController;
 
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        navController = NavHostFragment.findNavController(this);
 
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_sub_reddit_list, container, false);
+        closeBTN = v.findViewById(R.id.closeBTN);
+
+        closeBTN.setOnClickListener(this::toBackScreen);
         // Inflate the layout for this fragment
         return v;
+    }
+
+    private void toBackScreen(View view) {
+        navController.popBackStack();
     }
 }
--- app/src/main/java/cat/itb/clonreddit/fragments/TextPostFragment.java
+++ app/src/main/java/cat/itb/clonreddit/fragments/TextPostFragment.java
@@ -3,39 +3,48 @@ package cat.itb.clonreddit.fragments;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 
+import android.widget.ImageView;
 import android.widget.Spinner;
-
+import android.widget.TextView;
 
 
 import cat.itb.clonreddit.R;
 
 public class TextPostFragment extends Fragment {
-    private Spinner spinner;
+    private NavController navController;
+    private ImageView chooseCommunityArrowImageView;
+    private TextView chooseCommunityTextView;
+
 
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        navController = NavHostFragment.findNavController(this);
 
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_text_post, container, false);
+        chooseCommunityTextView = v.findViewById(R.id.chooseSubredditTextView);
+        chooseCommunityArrowImageView = v.findViewById(R.id.chooseSubredditArrow);
 
-
-        spinner = v.findViewById(R.id.spinner);
-
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getContext(), R.array.spinner_test, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
-
+        chooseCommunityTextView.setOnClickListener(this::chooseCommunityFragment);
+        chooseCommunityArrowImageView.setOnClickListener(this::chooseCommunityFragment);
         return v;
+    }
+
+
+    private void chooseCommunityFragment(View view) {
+        navController.navigate(R.id.action_textPostFragment_to_subRedditListFragment);
     }
 }
